a69937fd3484b72d82bec55c42bf5095ff55c996
Derek Gould <surfrdude11@gmail.com>
2017-11-17 14:24:51 -0800

    Add simple glob support

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import _path from 'path';
 import _fs from 'fs';
-import recursiveReaddirSync from 'recursive-readdir-sync';
 
 export default function (babel) {
     const { types: t } = babel;
@@ -22,12 +21,29 @@ export default function (babel) {
                 let filterNames = []; // e.g. A, B, C
                 
                 // has a /* specifing explicitly to use wildcard
-                let isExplicitWildcard = /\/\*$/.test(src);
+                const wildcardRegex = /\/([^\/]*\*[^\/]*)$/;
+                let isExplicitWildcard = wildcardRegex.test(src);
+                let filenameRegex = new RegExp('.+');
 
                 // in the above case we need to remove the trailing /*
                 if (isExplicitWildcard) {
-                    path.node.source.value = path.node.source.value.substring(0, src.length - 2);
-                    src = path.node.source.value;
+                    const lastSlash = path.node.source.value.lastIndexOf('/');
+                    src = path.node.source.value.substring(0, lastSlash);
+                    const filenameGlob = path.node.source.value.substring(lastSlash + 1);
+                    path.node.source.value = src;
+                    filenameRegex = filenameGlob.replace(/[*\(\[\)\]]/g, character => {
+                        switch(character) {
+                            case '*':
+                                return '.*';
+                            case '(':
+                            case ')':
+                            case '[':
+                            case ']':
+                                return '\\' + character;
+                        }
+                        return character;
+                    });
+                    filenameRegex = new RegExp(filenameRegex);
                 }
                 
 
@@ -37,7 +53,6 @@ export default function (babel) {
                 var files = [];
                 var dir = _path.join(_path.dirname(name), src); // path of the target dir.
 
-                const alreadyInitialized = {};
                 for (var i = node.specifiers.length - 1; i >= 0; i--) {
                     dec = node.specifiers[i];
                     
@@ -85,17 +100,19 @@ export default function (babel) {
                     
                     // Will throw if the path does not point to a dir
                     try {
-                        files = recursiveReaddirSync(dir)
-                            .map(file => file.replace(dir + '/', '')) // Handle shallow dependencies
-                            .map(file => file.replace(dir, ''))
-                            .filter(file =>
-                                exts.indexOf(_path.extname(file).substring(1)) > -1
-                            );
+                        let r = _fs.readdirSync(dir);
+                        for (var i = 0; i < r.length; i++) {
+                            // Check extension is of one of the aboves
+                            const {name, ext} = _path.parse(r[i]);
+                            if (exts.indexOf(ext.substring(1)) > -1 && filenameRegex.test(name)) {
+                                files.push(r[i]);
+                            }
+                        }
                     } catch(e) {
                         console.warn(`Wildcard for ${name} points at ${src} which is not a directory.`);
                         return;
                     }
-                    
+
                     // This is quite a mess but it essentially formats the file
                     // extension, and adds it to the object
                     for (var i = 0; i < files.length; i++) {
@@ -104,30 +121,35 @@ export default function (babel) {
                         let id = path.scope.generateUidIdentifier("wcImport");
                         
                         var file = files[i];
-                        var parts = file.split('/')
-                            // Set the fancy name based on options
-                            .map(part => getFancyName(part, state.opts))
-                            // Now we're 100% settled on the fancyName, if the user
-                            // has provided a filter, we will check it:
-                            .map(part => {
-                                if (filterNames.length > 0) {
-                                    // Find a filter name
-                                    let res = null;
-                                    for (let j = 0; j < filterNames.length; j++) {
-                                        if (filterNames[j].original === part) {
-                                            res = filterNames[j];
-                                            break;
-                                        }
-                                    }
-                                    if (res === null) return null;
-                                    return res.local;
-                                }
-                                return part;
-                            })
-                            .filter(part => part !== null);
+                        
+                        // Strip extension
+                        var fancyName = file.replace(/(?!^)\.[^.\s]+$/, "");
+                        
+                        // Handle dotfiles, remove prefix `.` in that case
+                        if (fancyName[0] === ".") {
+                            fancyName = fancyName.substring(1);
+                        }
+                        
+                        // If we're allowed to camel case, which is default, we run it
+                        // through this regex which converts it to a PascalCase variable.
+                        if (state.opts.noCamelCase !== true) {
+                            fancyName = fancyName.match(/[A-Z][a-z]+(?![a-z])|[A-Z]+(?![a-z])|([a-zA-Z\d]+(?=-))|[a-zA-Z\d]+(?=_)|[a-z]+(?=[A-Z])|[A-Za-z0-9]+/g).map(s => s[0].toUpperCase() + s.substring(1)).join("");
+                        }
 
-                        // If after filtering we have no parts left then continue
-                        if (parts.length === 0) continue;
+                        // Now we're 100% settled on the fancyName, if the user
+                        // has provided a filer, we will check it:
+                        if (filterNames.length > 0) {
+                            // Find a filter name
+                            let res = null;
+                            for (let j = 0; j < filterNames.length; j++) {
+                                if (filterNames[j].original === fancyName) {
+                                    res = filterNames[j];
+                                    break;
+                                }
+                            }
+                            if (res === null) continue;
+                            fancyName = res.local;
+                        }
                         
                         // This will remove file extensions from the generated `import`.
                         // This is useful if your src/ files are for example .jsx or
@@ -135,7 +157,7 @@ export default function (babel) {
                         // For situations like webpack you may want to disable this
                         var name;
                         if (state.opts.nostrip !== true) {
-                            name = "./" + _path.join(src, _path.dirname(file), _path.basename(file));
+                            name = "./" + _path.join(src, _path.basename(file));
                         } else {
                             name = "./" + _path.join(src, file);
                         }
@@ -143,9 +165,9 @@ export default function (babel) {
                         // Special behavior if 'filterNames'
                         if (filterNames.length > 0) {
                             let importDeclaration = t.importDeclaration(
-                                parts.map(part => t.importDefaultSpecifier(
-                                    t.identifier(part)
-                                )),
+                                [t.importDefaultSpecifier(
+                                    t.identifier(fancyName)
+                                )],
                                 t.stringLiteral(name)
                             );
                             path.insertAfter(importDeclaration);
@@ -160,70 +182,16 @@ export default function (babel) {
                             t.stringLiteral(name)
                         );
                         
-                        // Initialize the top level directories as an empty objects
-                        const nested = parts.slice(0, -1);
-                        nested.reduce((prev, curr) => {
-                            if (!prev) {
-                                if (alreadyInitialized[curr]) return {
-                                    path: curr,
-                                    member: alreadyInitialized[curr],
-                                };
-                                const member = t.memberExpression(
-                                    t.identifier(wildcardName),
-                                    t.stringLiteral(curr),
-                                    true
-                                );
-                                const setup = t.expressionStatement(
-                                    t.assignmentExpression("=", member, t.objectExpression([]))
-                                );
-                                path.insertBefore(setup);
-                                alreadyInitialized[curr] = member;
-                                return {
-                                    path: curr,
-                                    member,
-                                };
-                            }
-                            const newPath = `${prev.path}.${curr}`;
-                            if (alreadyInitialized[newPath]) return {
-                                path: newPath,
-                                member: alreadyInitialized[newPath],
-                            };
-                            const member = t.memberExpression(
-                                prev.member,
-                                t.stringLiteral(curr),
-                                true
-                            );
-                            const setup = t.expressionStatement(
-                                t.assignmentExpression("=", member, t.objectExpression([]))
-                            );
-                            path.insertBefore(setup);
-                            alreadyInitialized[newPath] = member;
-                            return {
-                                path: newPath,
-                                member,
-                            };
-                        }, null);
-
-                        // Chain the parts for access
-                        const accessPoint = parts.reduce((prev, curr) => {
-                            if (!prev) return t.memberExpression(
+                        // Assign it
+                        let thing = t.expressionStatement(
+                            t.assignmentExpression("=", t.memberExpression(
                                 t.identifier(wildcardName),
-                                t.stringLiteral(curr),
+                                t.stringLiteral(fancyName),
                                 true
-                            );
-                            return t.memberExpression(
-                                prev,
-                                t.stringLiteral(curr),
-                                true
-                            );
-                        }, null);
-
-                        // Assign the file to the parts
-                        let setter = t.expressionStatement(
-                            t.assignmentExpression("=", accessPoint, id)
-                        );
+                            ), id
+                        ));
                         
-                        path.insertAfter(setter);
+                        path.insertAfter(thing);
                         path.insertAfter(importDeclaration);
                     }
                     
@@ -234,21 +202,4 @@ export default function (babel) {
             }
         }
     };
-}
-
-function getFancyName(originalName, opts) {
-    // Strip extension
-    var fancyName = originalName.replace(/(?!^)\.[^.\s]+$/, "");
-
-    // Handle dotfiles, remove prefix `.` in that case
-    if (fancyName[0] === ".") {
-        fancyName = fancyName.substring(1);
-    }
-
-    // If we're allowed to camel case, which is default, we run it
-    // through this regex which converts it to a PascalCase variable.
-    if (opts.noCamelCase !== true) {
-        fancyName = fancyName.match(/[A-Z][a-z]+(?![a-z])|[A-Z]+(?![a-z])|([a-zA-Z\d]+(?=-))|[a-zA-Z\d]+(?=_)|[a-z]+(?=[A-Z])|[A-Za-z0-9]+/g).map(s => s[0].toUpperCase() + s.substring(1)).join("");
-    }
-    return fancyName;
 }
--- test/fixtures/filterTest/expected.js
+++ test/fixtures/filterTest/expected.js
@@ -7,3 +7,4 @@ var _A2 = _interopRequireDefault(_A);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 console.log((0, _A2.default)());
+
