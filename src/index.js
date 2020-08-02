69c69827b9ecce1f5189666d791c1a913ecb10d2
pmunin <philipp.munin@gmail.com>
2020-08-02 15:54:18 -0400

    update to babel 7

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ export default function (babel) {
     return {
         visitor: {
             ImportDeclaration(path, state) {
+                //console.log(path);
                 let node = path.node, dec;
                 var src = path.node.source.value;
 
@@ -50,7 +51,7 @@ export default function (babel) {
                 
 
                 // Get current filename so we can try to determine the folder
-                var name = this.file.parserOpts.sourceFileName || this.file.parserOpts.filename;
+                var name = this.file.opts.parserOpts.sourceFileName || this.file.opts.parserOpts.filename;
 
                 var files = [];
                 var dir = _path.join(_path.dirname(name), src); // path of the target dir.
--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
 var assert = require('assert');
-var babel = require('babel-core');
+var babel = require('@babel/core');
 var chalk = require('chalk');
 var clear = require('clear');
 var diff = require('diff');
 var fs = require('fs');
 var path = require('path');
 
-require('babel-register');
+require('@babel/register');
 
 var pluginPath = require.resolve('../src');
 
